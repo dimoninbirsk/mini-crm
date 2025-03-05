a7fb5a85a197f872e27a7645cc60b0e1bd1b03e2
dimoninbirsk <dimoninbirsk@gmail.com>
2025-03-05 19:23:27 +0500

    feat: create domain model v.1

---
+++ src/MiniCRM.Domain/Models/Discount.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniCRM.Domain.Models;
+
+public record Discount : BaseEntity
+{
+    public string Tag { get; set; } = string.Empty;
+    public decimal DiscountPercentage { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
---
+++ src/MiniCRM.Domain/Models/Expenses.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniCRM.Domain.Models;
+
+public record Expenses : BaseEntity
+{
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = string.Empty;
+    public decimal Cost { get; set; }
+    public int Count { get; set; } = 1;
+    public decimal TotalCost => Cost * Count;
+}
